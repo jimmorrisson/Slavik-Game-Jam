b5c8629c06f2993838c733b1afe21f863b84fec3
jimmmorrisson <pyszczakjakub@gmail.com>
2019-07-19 21:39:13 +0200

    Shooting added

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 20.0f;
+
+    [SerializeField]
+    public float destroyTime = 1.0f;
+
+    private Rigidbody bulletRigid;
+
+    void Start()
+    {
+        bulletRigid = GetComponent<Rigidbody>();
+        bulletRigid.velocity = transform.forward * speed;
+
+        Destroy(gameObject, destroyTime);
+    }
+}
