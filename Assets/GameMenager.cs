483b8402d1a9dd0baf98f4f3c6b97cf8982767df
jimmmorrisson <pyszczakjakub@gmail.com>
2019-07-19 19:44:06 +0200

    Game menager

---
+++ Assets/GameMenager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenager : MonoBehaviour
+{
+    [SerializeField]
+    public float time = 10;
+
+    public static GameMenager instance;
+
+    void Start()
+    {
+        if (instance != null)
+            instance = null;
+
+        instance = new GameMenager();
+    }
+    void Update()
+    {
+        time -= Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/TrashScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashScript : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+}
